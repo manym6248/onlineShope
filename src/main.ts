cc2e62974009093479fff059e9f5b675d8687996
manym6248 <mehrdad.moazzeny@gmail.com>
2023-04-28 15:01:29 +0330

    Basic settings quasar

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,19 @@
 import { createApp } from 'vue'
-import './style.css'
+import './assets/scss/main.scss'
 import App from './App.vue'
+import { Quasar } from 'quasar'
 
-createApp(App).mount('#app')
+// Import icon libraries
+import '@quasar/extras/roboto-font/roboto-font.css'
+import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/fontawesome-v5/fontawesome-v5.css'
+
+// Import Quasar css
+import 'quasar/src/css/index.sass'
+
+
+
+createApp(App).use(Quasar, {
+  plugins: {}, // import Quasar plugins and add here
+})
+.mount('#app')
